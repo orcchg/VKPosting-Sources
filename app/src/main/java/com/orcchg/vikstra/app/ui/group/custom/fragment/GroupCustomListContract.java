2386b9ae34d16a74814bce8bc33d76cc509ea580
Maxim Alov <orcchg@yandex.ru>
2017-02-15 16:17:00 +0300

    Group custom boilerplate added

---
+++ app/src/main/java/com/orcchg/vikstra/app/ui/group/custom/fragment/GroupCustomListContract.java
@@ -0,0 +1,4 @@
+package com.orcchg.vikstra.app.ui.group.custom.fragment;
+
+public interface GroupCustomListContract {
+}
---
+++ app/src/main/java/com/orcchg/vikstra/app/ui/group/custom/fragment/injection/GroupCustomListModule.java
@@ -0,0 +1,4 @@
+package com.orcchg.vikstra.app.ui.group.custom.fragment.injection;
+
+public class GroupCustomListModule {
+}
